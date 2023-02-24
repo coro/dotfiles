1b02dd3e4e0410518c7c86139243c76b2adf4e1c
Connor Rogers <23215294+coro@users.noreply.github.com>
2023-02-24 15:51:49 +0000

    Add basic keybinds

---
+++ config/nvim/lua/plugins/keybinds.lua
@@ -0,0 +1,6 @@
+return {
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" }),
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "[g]o to [r]eferences" }),
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[r]e[n]ame object" }),
+  vim.keymap.set("n", "<leader>km", ":Telescope keymaps<CR>", { desc = "[k]ey[m]aps" })
+}
