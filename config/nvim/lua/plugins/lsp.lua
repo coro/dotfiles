745be5e044fd2f0f3b61f523a5b86b99c27a0263
Connor Rogers <23215294+coro@users.noreply.github.com>
2023-02-24 14:13:36 +0000

    Enable LSP and auto complete

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,14 @@
 return {
 	{
-		"neovim/nvim-lspconfig"
+		"neovim/nvim-lspconfig",
+		config = function()
+			local opts = { capabilities = require("cmp_nvim_lsp").default_capabilities() }
+			require'lspconfig'.gopls.setup(opts)
+			require'lspconfig'.terraformls.setup(opts)
+		end,
+		keys = {
+			{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+		},
 	},
+
 }
