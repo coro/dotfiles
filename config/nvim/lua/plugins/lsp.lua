7be556cfebfd294fbb77a718b5146a74fdae627e
Connor Rogers <23215294+coro@users.noreply.github.com>
2023-03-01 10:58:08 +0000

    gofmt and organise imports on go file saves

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,34 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local opts = { capabilities = require("cmp_nvim_lsp").default_capabilities() }
-      require'lspconfig'.gopls.setup(opts)
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      require'lspconfig'.gopls.setup {
+        opts,
+        on_attach = function(_, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+              params.context = {only = {"source.organizeImports"}}
+
+              local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+              for _, res in pairs(result or {}) do
+                for _, r in pairs(res.result or {}) do
+                  if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+                  else
+                    vim.lsp.buf.execute_command(r.command)
+                  end
+                end
+              end
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+
+      }
       require'lspconfig'.terraformls.setup(opts)
       require'lspconfig'.clojure_lsp.setup(opts)
       require'lspconfig'.lua_ls.setup(opts)
