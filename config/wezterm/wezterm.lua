026855b2148923283f7a11b8e7b6ec75e4be65d9
Connor Rogers <23215294+coro@users.noreply.github.com>
2023-03-03 16:10:47 +0000

    Migrate to wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require("wezterm")
+return {
+  font = wezterm.font 'JetBrains Mono',
+  font_size = 14.0,
+  background = {
+    {
+      source = {
+        File = wezterm.config_dir .. '/Coriolis_Station.png'
+      },
+      hsb = {
+        brightness = 0.05
+      },
+      horizontal_align = "Center",
+      vertical_align = "Middle",
+    },
+  },
+  leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 },
+  keys = {
+    {
+      key = 'c',
+      mods = 'LEADER',
+      action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+    },
+    {
+      key = '-',
+      mods = 'LEADER',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '_',
+      mods = 'LEADER',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+  },
+}
