ace1eb22f1b99cd410c198ce309991b7f061ef4a
Connor Rogers <23215294+coro@users.noreply.github.com>
2024-01-31 12:37:12 +0000

    Upgrade to indent-blankline v3

--- config/nvim/lua/plugins/appearance.lua
+++ config/nvim/lua/plugins/appearance.lua
@@ -11,10 +11,7 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = "ibl",
   },
   {
     'nvim-lualine/lualine.nvim',
