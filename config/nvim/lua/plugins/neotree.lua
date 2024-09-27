3f280705443a1324e80b413f038c8ba313f082d3
Connor Rogers <23215294+coro@users.noreply.github.com>
2024-09-27 16:11:52 +0100

    Enable follow file and snappier file watcher in neotree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -10,4 +10,12 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
+	opts = {
+		filesystem = {
+			use_libuv_file_watcher = true,
+			follow_current_file = {
+				enabled = true,
+			},
+		},
+	},
 }
